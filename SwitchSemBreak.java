922ecc770069146d17f6a0bce46ff98b3d53aca5
Fabio dos Santos Mota <96454065+FabioMota30@users.noreply.github.com>
2022-05-22 21:44:21 -0300

    Switch
    Usando a função Switch sem o Break.

---
+++ SwitchSemBreak.java
@@ -0,0 +1,33 @@
+package controle;
+
+public class SwitchSemBreak {
+	
+	public static void main(String[] args) {
+		
+		//sem o break o programa executa todos os abaixos caso tiver
+		
+		String faixa = "preta";
+		
+		switch(faixa.toLowerCase()) {
+		case "preta":
+			System.out.println("Sei o Bassai-Dai...");
+		case "marrom":
+			System.out.println("Sei o Tekki Shodan");
+		case "roxa":
+			System.out.println("Sei o Heian Godan");
+		case "verde":
+			System.out.println("Sei o Heidan Yodan");
+		case "laranja":
+			System.out.println("Sei o Heian Sandan");
+		case "vermelha":
+			System.out.println("Sei o Heian Nidan");
+		case "amarela":
+			System.out.println("Sei o Heidan Shodan");
+		//default:
+				//System.out.println("Não sei nada");
+			
+		}
+	
+	}
+
+}
